6c84b474142012dfbca1b09b99baa9fa7771b46b
abhinav_2008 <abhinavraj1090@gmail.com>
2026-01-06 18:36:41 +0530

    Refactor calculator to use Equation_list for calculations
    Refactored calculator logic to use Equation_list for operations and improved handling of input parsing. 
    
    Last Versions couldn't handle -ves well, so it needed an update

--- Practice07(Calculator).py
+++ Practice07(Calculator).py
@@ -1,71 +1,77 @@
 Equation = input("Equation [ODMAS]: ").replace(" ", "").lower()
 
-#Funtions
-
-def multiply(String1,String2):
-    return (int(String1) * int(String2))
-def divide(String1,String2):
-    return (int(String1) / int(String2))
-def add(String1,String2):
-    return (int(String1) + int(String2))
-def subtract(String1,String2):
-    return (int(String1) - int(String2))
+#Functions
 
 def isPresent(Str,List):
     if Str in List:
         return True
-    
-def Calculate(symbol,function):
-    while isPresent(symbol,Symbols):
-        for i in range(0,len(Symbols)):
-            if Symbols[i] == symbol:
-                n = function(numbers[i],numbers[i+1])
-                numbers[i] = n
-                numbers.pop(i+1)
-                break
-        for i in range(0,len(Symbols)):
-            if Symbols[i] == symbol:
-                Symbols.pop(i)    
-                break 
 
-
-#Finding Symbols
-Symbols = list()
-for i in range(0,len(Equation)):
-    if (Equation[i] == "+" or Equation[i] == "-" or Equation[i] == "*" or Equation[i] == "/"):
-        Symbols.append(Equation[i])
-        
-    elif (Equation[i] == "o" and (Equation[i+1] == "f")):
-        Symbols.append("*")
-        
 #Finding Numbers
 digits= ["0","1","2","3","4",'5','6','7','8',"9"]
-numbers = list()
 number = list()
+
+#Equation LIST
+
+Equation_list = list()
+
 for i in range(0,len(Equation)+1):
     if i == len(Equation):
         number_string = ''.join(number)
-        numbers.append(number_string)
+        Equation_list.append(number_string)
         number = list()
+        break
 
     elif Equation[i] in digits:
         number.append(Equation[i])
-
-    else:
+    elif Equation[i] not in digits:
         number_string = ''.join(number)
         if number_string != '':
-            numbers.append(number_string)
+            Equation_list.append(number_string)
         number = list()
+
+    if Equation[i] == "+" or Equation[i] == "-" or (Equation[i] == "*" or Equation[i] == "/") :
+        Equation_list.append(Equation[i])
+        
+    if (Equation[i] == "o" and Equation[i+1] == "f"):
+        Equation_list.append("*")
+
+    
 
 
 #Calcultaions:-
 
-Calculate("*",multiply)
-Calculate("/",divide)
-Calculate("+",add)
-Calculate("-",subtract)
 
 
-print(numbers[0])
+while isPresent("+",Equation_list) or isPresent("-",Equation_list):
+    for i in range(0,len(Equation_list)-1):
+        if Equation_list[i] == "+" or Equation_list[i] == "-":
+            Equation_list[i] = Equation_list[i] + Equation_list[i+1]
+            Equation_list.pop(i+1)
+            break
+  
 
+
+while isPresent("*",Equation_list) or isPresent("/",Equation_list):
+    for i in range(0,len(Equation_list)-1):
+        
+        
+        if Equation_list[i] == "/":
+            Equation_list[i] = float(Equation_list[i-1]) / float(Equation_list[i+1])
+            Equation_list.pop(i+1)
+            Equation_list.pop(i-1)
+            break
+    
+        if Equation_list[i] == "*":
+            Equation_list[i] = float(Equation_list[i-1]) * float(Equation_list[i+1])
+            Equation_list.pop(i+1)
+            Equation_list.pop(i-1)
+            break
+
+
+while len(Equation_list) >1:
+    Equation_list[0] = float(Equation_list[0]) + float(Equation_list[1])
+    Equation_list.pop(1)
+    
+Solution = Equation_list[0]
+print(Solution)
 #noice
